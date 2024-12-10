1df8c5210067dac229b74a58fbaa0c3c23ba59b0
ajayygurjar <ajay1651@gmail.com>
2024-12-10 12:33:31 +0530

    Rendering Expenses

--- src/App.js
+++ src/App.js
@@ -1,48 +1,27 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
+const App = () => {
   const expenses = [
-    {
-      id: "1",
-      date: new Date(2023, 7, 15),
-      title: "Insurance",
-      price: 100,
-      location: "Bangalore",
-    },
-    {
-      id: "2",
-      date: new Date(2023, 3, 25),
-      title: "Book",
-      price: 10,
-      location: "Delhi",
-    },
-    {
-      id: "3",
-      date: new Date(2023, 10, 11),
-      title: "Pen",
-      price: 1,
-      location: "Hyderabad",
-    },
-    {
-      id: "4",
-      date: new Date(2023, 1, 14),
-      title: "Laptop",
-      price: 200,
-      location: "Mumbai",
-    },
+    { id: 1, title: "Insurance", date: new Date(2023, 7, 15), price: 100 },
+    { id: 2, title: "Book", date: new Date(2023, 8, 25), price: 10 },
+    { id: 3, title: "Pen", date: new Date(2023, 2, 10), price: 1 },
+    { id: 4, title: "Laptop", date: new Date(2023, 9, 17), price: 200 },
   ];
-
-  const dataFromNewExpense = (expenseData) => {
-    console.log(expenseData);
+  const [addExpense, setAddExpense] = useState(expenses);
+  const addExpenseHandler = (expense) => {
+    setAddExpense((prevExpense) => {
+      return [...prevExpense, expense];
+    });
   };
 
   return (
     <div>
-      <NewExpense onAddExpense={dataFromNewExpense} />
-      <Expenses expenses={expenses}></Expenses>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses expenses={addExpense} />
     </div>
   );
-}
+};
 
 export default App;
