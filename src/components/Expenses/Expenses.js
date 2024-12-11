7d7f7b228f1dc51f12772d20b7a32b17c48e5f92
ajayygurjar <ajay1651@gmail.com>
2024-12-11 17:45:52 +0530

    Creating a controlled component - The Expenses Filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,27 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+  const [filterYear, setFilterYear] = useState("2023");
+
+  const changeFilterHandler = (selectedYear) => {
+    setFilterYear(selectedYear);
+  };
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filterYear;
+  });
+
   return (
     <Card className="expenses">
-      {props.expenses.map((expense) => {
+      <ExpensesFilter
+        selected={filterYear}
+        onChangeFilter={changeFilterHandler}
+      />
+      {filteredExpenses.map((expense) => {
         return (
           <ExpenseItem
             key={expense.id}
